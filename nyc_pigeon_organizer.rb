2d2833df358838c2db75b655721f6da939d2bec6
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-10 18:51:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -60,21 +60,21 @@ pigeon_data = {
 
 # nyc_pigeon_organizer(pigeon_data)
 
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key,value|
-    value.each do |new_value,names|
-      names.each do |name|
+# def nyc_pigeon_organizer(data)
+#   new_hash = {}
+#   data.each do |key,value|
+#     value.each do |new_value,names|
+#       names.each do |name|
         
-  if !new_hash[name]
-    new_hash[name] = {}
-  end
-  if new_hash[name][key]
-    new_hash[name][key] = []
-  end
-     new_hash[name][key] << new_value.to_s
-    end
-  end
-end
-    new_hash
-  end
+#   if !new_hash[name]
+#     new_hash[name] = {}
+#   end
+#   if new_hash[name][key]
+#     new_hash[name][key] = []
+#   end
+#     new_hash[name][key] << new_value.to_s
+#     end
+#   end
+# end
+#     new_hash
+#   end
